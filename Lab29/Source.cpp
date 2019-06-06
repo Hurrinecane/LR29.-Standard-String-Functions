12c1ccbece088b6927735478c562c32681410763
Hurrinecane <Hurrinecane@desktop>
2019-06-06 17:18:04 +0300

    a

--- Lab29/Source.cpp
+++ Lab29/Source.cpp
@@ -6,23 +6,25 @@
 #include<iostream>
 #include <conio.h>
 
-int main()
-{
-	setlocale(LC_ALL, "");
-	int length = 256;
-	char *s = (char*)malloc(length * sizeof(char));
-	gets_s(s, length);
-	char *parenthesis1 = 0;
-	char *parenthesis2 = 0;
-#pragma region 1
+int length = 256;
+char *s = (char*)malloc(length * sizeof(char));
+char *parenthesis1 = NULL;
+char *parenthesis2 = NULL;
 
-
+void nine()
+{	
 	do
 	{
-
-		int len = strlen(s);
 		parenthesis1 = strchr(s, '(');
 		parenthesis2 = strchr(s, ')');
+
+		if (parenthesis1 == 0 || parenthesis2 == 0)
+			break;
+
+		if (*parenthesis2 > *parenthesis1)
+			parenthesis2 = strchr(&parenthesis2[1], ')');
+
+		int len = strlen(s);
 		if (parenthesis1 && parenthesis2 && parenthesis1 < parenthesis2) //mozno li?
 		{
 			char *s1 = (char*)calloc(length, sizeof(char));
@@ -36,13 +38,52 @@ int main()
 				}
 			}
 			strncat_s(s1, length, s, n);
-			len = strlen(s1);
 			strcat_s(s1, length, parenthesis2 + 1);
 			strcpy_s(s, length, s1);
 		}
-	} while (parenthesis1 != 0 && parenthesis2 != 0);
-	puts(s);
-	system("pause");
+
+		puts(s);
+
+	} while (true);
+
+
+	_getch();
+}
+
+int main()
+{
+	setlocale(LC_ALL, "");
+
+	gets_s(s, length);
+
+	nine();
+
+#pragma region 2
+
+	int quantity = 0, max = 0;
+	int len = strlen(s);
+
+	for (int i = 0; i < len; i++)
+	{
+		bool space = false;
+
+		if (isspace(s[i]))
+			space = true;
+
+		if (space)
+		{
+			quantity++;
+		}
+		else
+		{
+			if (quantity > max) max = quantity;
+			quantity = 0;
+		}
+
+	}
+
+	printf_s("Max quantity: %d\n", max);
+	_getch();
 
 #pragma endregion
 
